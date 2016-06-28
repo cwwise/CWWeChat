55858e48f634635360d0f860facac1bb3937b6fc
chenwei <wei18810109052@163.com>
2016-06-28 11:19:33 +0800

    添加文件

---
+++ CWWeChat/Expand/Extension/UIStoryboard+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIStoryboard+Extension.swift
+//  CWWeChat
+//
+//  Created by chenwei on 16/6/28.
+//  Copyright © 2016年 chenwei. All rights reserved.
+//
+
+import UIKit
+
+class UIStoryboard_Extension: UIStoryboard {
+
+}
