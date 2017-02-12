4a4f241dd908acc274379d6fba51e405134158a0
ios_chenwei <wei18810109052@163.com>
2017-02-12 23:04:17 +0800

    1. 使用新的方式来实现xmpp部分

---
+++ CWWeChat/CommonModule/CWChatClient/XMPP/CWXMPPManager.swift
@@ -0,0 +1,16 @@
+//
+//  CWXMPPManager.swift
+//  CWWeChat
+//
+//  Created by chenwei on 2017/2/12.
+//  Copyright © 2017年 chenwei. All rights reserved.
+//
+
+import UIKit
+
+/// xmpp管理实例
+//class CWXMPPManager: NSObject {
+//    
+//    
+//    
+//}
