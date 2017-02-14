83208e13c321a3219ab672bcd434db27d878f17e
chenwei <wei18810109052@163.com>
2017-02-14 22:04:52 +0800

    1. 添加ChatClient配置

---
+++ CWWeChat/CommonModule/CWChatClient/XMPP/CWMessageHandleProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  CWMessageHandleProtocol.swift
+//  CWWeChat
+//
+//  Created by wei chen on 2017/2/14.
+//  Copyright © 2017年 chenwei. All rights reserved.
+//
+
+import Foundation
+
+
+/// 消息处理协议
+protocol CWMessageHandleProtocol: NSObjectProtocol {
+    
+    
+}
