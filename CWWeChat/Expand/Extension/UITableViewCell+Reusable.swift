0142a1dea4dafa177270066d68816b979d3d059c
ios_chenwei <wei18810109052@163.com>
2017-03-27 00:36:47 +0800

    完善会话列表

---
+++ CWWeChat/Expand/Extension/UITableViewCell+Reusable.swift
@@ -0,0 +1,13 @@
+//
+//  UITableViewCell+Reusable.swift
+//  CWWeChat
+//
+//  Created by chenwei on 2017/3/27.
+//  Copyright © 2017年 cwcoder. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var identifier: String { return String(describing: self) }
+}
