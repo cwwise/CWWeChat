68dad03c1c74a5760ccd0ea6f8cbaca1c2c04c2d
chenwei <wei18810109052@163.com>
2017-04-04 21:35:37 +0800

    修改travis的问题

---
+++ CWWeChatTests/CWWeChat_ChatStoreTests.swift
@@ -0,0 +1,22 @@
+//
+//  CWWeChat_ChatStoreTests.swift
+//  CWWeChat
+//
+//  Created by wei chen on 2017/4/4.
+//  Copyright © 2017年 cwcoder. All rights reserved.
+//
+
+import XCTest
+@testable import CWWeChat
+import Quick
+import Nimble
+
+class RGBTests: QuickSpec {
+    override func spec() {
+        describe("RGB") {
+            it("works") {
+                expect(true).to(beTrue())
+            }
+        }
+    }
+}
