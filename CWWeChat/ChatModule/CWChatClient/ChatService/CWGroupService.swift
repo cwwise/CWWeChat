4a667948a1f56ca129f0bab96903de197daedc1d
ios_chenwei <wei18810109052@163.com>
2017-04-05 18:23:55 +0800

    使用XMPPMUCLight来实现

--- CWWeChat/ChatModule/CWChatClient/ChatService/CWGroupService.swift
+++ CWWeChat/ChatModule/CWChatClient/ChatService/CWGroupService.swift
@@ -11,8 +11,15 @@ import XMPPFramework
 
 class CWGroupServic: XMPPModule {
     
+    lazy var mucLight: XMPPMUCLight = {
+        let mucLight = XMPPMUCLight(dispatchQueue: self.moduleQueue)
+        mucLight?.activate(CWChatXMPPManager.share.xmppStream)
+        return mucLight!
+    }()
+    
     override init!(dispatchQueue queue: DispatchQueue!) {
         super.init(dispatchQueue: queue)
+        self.mucLight.addDelegate(self, delegateQueue: self.moduleQueue)
     }
     
 }
@@ -28,23 +35,32 @@ extension CWGroupServic: CWGroupManager {
     
     func fetchJoinGroups() {
         
-        let queryElement = DDXMLElement(name: "query", xmlns: "http://jabber.org/protocol/disco#items")
+//        let queryElement = DDXMLElement(name: "query", xmlns: "http://jabber.org/protocol/disco#items")
+//        
+//        let iqElement = DDXMLElement(name: "iq")
+//        iqElement.addAttribute(withName: "type", stringValue: "get")
+//        let chatClient = CWChatClient.share
+//        
+//        iqElement.addAttribute(withName: "from", stringValue: "chenwei@hellochatim.p1.im")
+//        iqElement.addAttribute(withName: "to", stringValue: "conference.hellochatim.p1.im")
+//        iqElement.addAttribute(withName: "id", stringValue: "getexistroomid")
+//
+//        iqElement.addChild(queryElement!)
+//        log.debug(iqElement)
+//        CWChatXMPPManager.share.xmppStream.send(iqElement)
         
-        let iqElement = DDXMLElement(name: "iq")
-        iqElement.addAttribute(withName: "type", stringValue: "get")
-        let chatClient = CWChatClient.share
-        
-        iqElement.addAttribute(withName: "from", stringValue: "chenwei@hellochatim.p1.im")
-        iqElement.addAttribute(withName: "to", stringValue: "conference.hellochatim.p1.im")
-        iqElement.addAttribute(withName: "id", stringValue: "getexistroomid")
-
-        iqElement.addChild(queryElement!)
-        log.debug(iqElement)
-        CWChatXMPPManager.share.xmppStream.send(iqElement)
+        mucLight.discoverRooms(forServiceNamed: "conference.hellochatim.p1.im")
     }
     
     
     
     
     
+}
+
+extension CWGroupServic: XMPPMUCLightDelegate {
+    func xmppMUCLight(_ sender: XMPPMUCLight, didDiscoverRooms rooms: [DDXMLElement], forServiceNamed serviceName: String) {
+        log.debug(serviceName)
+        log.debug(rooms)
+    }
 }
